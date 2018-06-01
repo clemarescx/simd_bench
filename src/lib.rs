39955dab6b38f3fb26eada03583ef68a34b1ac40
Clement Marescaux <marcle15@student.westerdals.no>
2018-06-01 08:06:23 +0200

    Shorter test array. Interestingly, parallel methods are the slowest now!

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,15 @@ pub fn add_two_via_func(v: &mut Vec<i32>) {
    } 
 }
 
+pub fn add_two_via_lambda<T: Fn(i32)->i32>(v: &mut Vec<i32>, f: T){
+   for i in 0..v.len() {
+       v[i] = f(v[i]); 
+   } 
+}
+
 pub fn add_two_inline(v: &mut Vec<i32>) {
    for i in 0..v.len() {
-       v[i] = v[i] + 2; 
+       v[i] += 2; 
    } 
 }
 
@@ -36,7 +42,7 @@ pub fn add_two_par_iter_slice(v: &mut [i32]) {
 mod tests {
     use super::*;
     use test::Bencher;
-
+    
     #[test]
     fn it_works() {
         assert_eq!(4, add_two(2));
@@ -49,31 +55,37 @@ mod tests {
 
     #[bench]
     fn bench_simd_via_func(b: &mut Bencher) { 
-        let mut v : Vec<i32> = vec![1; 1_000_000];
+        let mut v : Vec<i32> = vec![1; 100_000];
         b.iter(|| add_two_via_func(&mut v));
     }
 
     #[bench]
+    fn bench_simd_via_lambda(b: &mut Bencher) { 
+        let mut v : Vec<i32> = vec![1; 100_000];
+        b.iter(|| add_two_via_lambda(&mut v, |x| x+2 ) );
+    }
+    
+    #[bench]
     fn bench_simd_inline(b: &mut Bencher) { 
-        let mut v : Vec<i32> = vec![1; 1_000_000];
+        let mut v : Vec<i32> = vec![1; 100_000];
         b.iter(|| add_two_inline(&mut v));
     }
 
     #[bench]
     fn bench_simd_iter(b: &mut Bencher) { 
-        let mut v : Vec<i32> = vec![1; 1_000_000];
+        let mut v : Vec<i32> = vec![1; 100_000];
         b.iter(|| add_two_iter(&mut v));
     }
 
     #[bench]
-    fn bench_simd_par_iter(b: &mut Bencher) { 
-        let mut v : Vec<i32> = vec![1; 1_000_000];
+    fn bench_simd_par_iter_vec(b: &mut Bencher) { 
+        let mut v : Vec<i32> = vec![1; 100_000];
         b.iter(|| add_two_par_iter_vec(&mut v));
     }
 
     #[bench]
     fn bench_simd_par_iter_slice(b: &mut Bencher) { 
-        let mut v : Vec<i32> = vec![1; 1_000_000];
+        let mut v : Vec<i32> = vec![1; 100_000];
         b.iter(|| add_two_par_iter_slice(&mut v));
     }
 }
